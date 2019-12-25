0d92dd48d67846a3bf12cd10ad8b120e55d1cf2b
rezga12 <rmeis16@freeuni.edu.ge>
2019-12-25 13:22:51 +0400

    Updates

---
+++ precache-manifest.1795993f238819d740da543cf52d3ca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6441150156355df50308b66d69c17c",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "9a9007ce5eb797c75b2e",
+    "url": "/gameoflife/static/css/main.46e85e67.chunk.css"
+  },
+  {
+    "revision": "2b29af2a607a88213a2f",
+    "url": "/gameoflife/static/js/2.1a1ab687.chunk.js"
+  },
+  {
+    "revision": "9a9007ce5eb797c75b2e",
+    "url": "/gameoflife/static/js/main.e52073f6.chunk.js"
+  },
+  {
+    "revision": "37b17cfd8c77716f2b77",
+    "url": "/gameoflife/static/js/runtime~main.050aefe4.js"
+  }
+]);
